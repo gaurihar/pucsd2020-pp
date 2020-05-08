3341da1d8c2fc8cd2dadf25146ae39b647b41cae
gaurihar <gaurihardarade@gmail.com>
2020-05-08 22:20:49 +0530

    create tables for acl project

--- Access_Control_System_Managment/src/backend/acl/resource/sql/001_create_table.sql
+++ Access_Control_System_Managment/src/backend/acl/resource/sql/001_create_table.sql
@@ -1,12 +1,10 @@
 USE ACL;
 
-CREATE TABLE IF NOT EXISTS  type_of_user(
-user_type varchar(20) PRIMARY KEY
-)
+
 
 CREATE TABLE IF NOT EXISTS user_info (
     user_id                  INT         AUTO_INCREMENT      PRIMARY KEY,
-    user_type           VARCHAR(20)   NOT NULL,
+    user_is_root         TINYINT(1)   NOT NULL,
     first_name          VARCHAR(25)    NOT NULL,
     last_name           VARCHAR(25)    NOT NULL,
     email               VARCHAR(64)    NOT NULL UNIQUE,
@@ -15,9 +13,7 @@ CREATE TABLE IF NOT EXISTS user_info (
     creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
     last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     updated_by          INT         NOT NULL DEFAULT 0,
-    deleted             TINYINT(1)  NOT NULL DEFAULT 0,
-    FOREIGN KEY(user_type) REFERENCES type_of_user(user_type) On UPDATE CASCADE On DELETE CASCADE
-
+    deleted             TINYINT(1)  NOT NULL DEFAULT 0
 )ENGINE = INNODB CHARACTER SET=utf8;
 
 CREATE TABLE IF NOT EXISTS groups (
@@ -28,7 +24,7 @@ CREATE TABLE IF NOT EXISTS groups (
     last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     updated_by          INT         NOT NULL DEFAULT 0,
     deleted             TINYINT(1)  NOT NULL DEFAULT 0,
-    FOREIGN KEY(group_owner) REFERENCES user_info(user_id) On UPDATE CASCADE On DELETE CASCADE
+    FOREIGN KEY(group_owner) REFERENCES user_info(user_id)
     
 )ENGINE = INNODB CHARACTER SET=utf8;
 
@@ -38,9 +34,18 @@ CREATE TABLE IF NOT EXISTS permission (
     permission_info varchar(20)
 )ENGINE = INNODB CHARACTER SET=utf8;
 
+CREATE TABLE IF NOT EXISTS resources_type(
+resource_type_id INT  PRIMARY KEY,
+r_discription varchar(50) NOT NULL
+)ENGINE = INNODB CHARACTER SET=utf8;
+
+
+
 CREATE TABLE IF NOT EXISTS resources(
     resources_id            INT        AUTO_INCREMENT      PRIMARY KEY,
+    resource_type_id    INT    NOT NULL,
     resource_name    VARCHAR(50)    NOT NULL, 
+    resources_parent VARCHAR(50) NOT NULL,
     resource_path    VARCHAR(100)    NOT NULL, 
     creation_date       DATETIME    DEFAULT CURRENT_TIMESTAMP,
     last_update         DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -52,8 +57,8 @@ CREATE TABLE IF NOT EXISTS resources(
 CREATE TABLE IF NOT EXISTS user_group (
     user_id             INT  NOT NULL,        
     groups_id            INT NOT NULL,        
-    FOREIGN KEY(user_id) REFERENCES user_info(user_id) On UPDATE CASCADE On DELETE CASCADE,
-    FOREIGN KEY(groups_id) REFERENCES groups(groups_id) On UPDATE CASCADE On DELETE CASCADE,
+    FOREIGN KEY(user_id) REFERENCES user_info(user_id) ,
+    FOREIGN KEY(groups_id) REFERENCES groups(groups_id) ,
     PRIMARY KEY(user_id,groups_id)
 )ENGINE = INNODB CHARACTER SET=utf8;
 
@@ -62,9 +67,9 @@ CREATE TABLE IF NOT EXISTS user_permission
 user_id INT NOT NULL,
 resources_id INT NOT NULL,
 permission_num INT NOT NULL,
-FOREIGN KEY(user_id) REFERENCES user_info(user_id) On UPDATE CASCADE On DELETE CASCADE,
-FOREIGN KEY(resources_id) REFERENCES resources(resources_id) On UPDATE CASCADE On DELETE CASCADE,
-FOREIGN KEY(permission_num) REFERENCES permission(permission_num) On UPDATE CASCADE On DELETE CASCADE,
+FOREIGN KEY(user_id) REFERENCES user_info(user_id) ,
+FOREIGN KEY(resources_id) REFERENCES resources(resources_id),
+FOREIGN KEY(permission_num) REFERENCES permission(permission_num) ,
 PRIMARY KEY(user_id,resources_id,permission_num)
 )ENGINE = INNODB CHARACTER SET=utf8;
 
@@ -74,8 +79,8 @@ CREATE TABLE IF NOT EXISTS group_permission
 groups_id INT  NOT NULL,
 resources_id INT  NOT NULL,
 permission_num INT NOT NULL,
-FOREIGN KEY(groups_id) REFERENCES groups(groups_id) On UPDATE CASCADE On DELETE CASCADE,
-FOREIGN KEY(resources_id) REFERENCES resources(resources_id) On UPDATE CASCADE On DELETE CASCADE,
-FOREIGN KEY(permission_num) REFERENCES permission(permission_num) On UPDATE CASCADE On DELETE CASCADE,
+FOREIGN KEY(groups_id) REFERENCES groups(groups_id) ,
+FOREIGN KEY(resources_id) REFERENCES resources(resources_id) ,
+FOREIGN KEY(permission_num) REFERENCES permission(permission_num),
 PRIMARY KEY(groups_id,resources_id,permission_num)
 )ENGINE = INNODB CHARACTER SET=utf8;
