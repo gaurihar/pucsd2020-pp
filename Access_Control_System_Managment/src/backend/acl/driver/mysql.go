72fe60767be371692f0ba9643e9f135c4091d11e
gaurihar <gaurihardarade@gmail.com>
2020-05-16 20:51:54 +0530

    getAll function work properly

--- Access_Control_System_Managment/src/backend/acl/driver/mysql.go
+++ Access_Control_System_Managment/src/backend/acl/driver/mysql.go
@@ -32,24 +32,19 @@ func NewMysqlConnection(cfg config.MysqlConnection) (*sql.DB, error) {
 	}
 
 	if cfg.IdleConnection > 0 {
-		//log.Printf("Checking Idle connection")
-
 		db.SetMaxIdleConns(cfg.IdleConnection)
 	}
 	if cfg.MaxConnection > 0 {
-		//log.Printf("Checking max connection")
-
+		
 		db.SetMaxOpenConns(cfg.MaxConnection)
 	}
-	//log.Printf("Setting Connection lifeline")
-
+	
 	db.SetConnMaxLifetime(time.Second * CONN_MAX_LIFETIME)
 
 	if err := db.Ping(); err != nil {
 		log.Fatalf("Failed to ping mysql: %v", err)
 	}
-	//log.Printf("return either database driver connection ")
-
+	
 	return db, err
 }
 
@@ -59,7 +54,6 @@ func GetPlaceHolder(count int) string {
 		str := strings.Repeat("?, ", count)
 		return str[:len(str)-2]
 	}
-
 	return ""
 }
 
@@ -68,8 +62,7 @@ func GetPlaceHolder(count int) string {
  */
 func Create(conn *sql.DB, object model.IModel) (sql.Result, error) {
 	
-	//log.Printf("Reading object values from interface type using reflection")
-
+	
 	rValue := reflect.ValueOf(object)
 	rType := reflect.TypeOf(object)
 	columns := []string{}
@@ -89,8 +82,6 @@ func Create(conn *sql.DB, object model.IModel) (sql.Result, error) {
 		params = append(params, value.Interface())
 		count++
 	}
-   //log.Printf("Creating query string ...")
-
 	var queryBuffer bytes.Buffer
 	queryBuffer.WriteString("INSERT INTO ")
 	queryBuffer.WriteString(object.Table())
@@ -109,16 +100,12 @@ func Create(conn *sql.DB, object model.IModel) (sql.Result, error) {
 	}
 
 	defer stmt.Close()
-	//log.Printf("Executing query string ...")
-	
 	result, err := stmt.Exec(params...)
 	if nil != err {
 		log.Printf("Insert Execute Error: %s\nError Query: %s : %s\n",
 			err.Error(), object.String(), query)
 		return nil, err
 	}
-	//log.Printf("Returning result string ...")
-
 	return result, nil
 }
 
@@ -126,9 +113,6 @@ func Create(conn *sql.DB, object model.IModel) (sql.Result, error) {
  * Update existing row with key column
  */
 func UpdateById(conn *sql.DB, object model.IModel) error {
-	//log.Printf("Reading object values to update from interface type using reflection")
-
-
 	rValue := reflect.ValueOf(object)
 	rType := reflect.TypeOf(object)
 
@@ -172,7 +156,6 @@ func UpdateById(conn *sql.DB, object model.IModel) error {
 	queryBuffer.WriteString(";")
 
 	query := queryBuffer.String()
-	//	log.Println("Update statement is: %s", query)
 	stmt, err := conn.Prepare(query)
 	if nil != err {
 		log.Printf("Update Syntax Error: %s\n\tError Query: %s : %s\n",
@@ -261,9 +244,6 @@ func UpdateByEmail(conn *sql.DB, object model.IModel) error {
 
 
 func GetById(conn *sql.DB, object model.IModel, id int64) (model.IModel, error) {
-	//log.Printf("Reading object values from interface type using reflection")
-
-
 	rValue := reflect.ValueOf(object)
 	rType := reflect.TypeOf(object)
 
@@ -290,7 +270,6 @@ func GetById(conn *sql.DB, object model.IModel, id int64) (model.IModel, error) 
 	queryBuffer.WriteString(" WHERE id = ?")
 
 	query := queryBuffer.String()
-	//	log.Printf("GetById sql: %s\n", query)
 	row, err := conn.Query(query, id)
 
 	if nil != err {
@@ -317,14 +296,12 @@ func GetById(conn *sql.DB, object model.IModel, id int64) (model.IModel, error) 
 	return object, nil
 }
 
+
+
 func GetAll(conn *sql.DB, object model.IModel, limit, offset int64) ([]interface{}, error) {
-	
 	rValue := reflect.ValueOf(object)
 	rType := reflect.TypeOf(object)
-
 	columns := []string{}
-	pointers := make([]interface{}, 0)
-
 	for idx := 0; idx < rValue.Elem().NumField(); idx++ {
 		field := rType.Elem().Field(idx)
 		if COLUMN_INGNORE_FLAG == field.Tag.Get("ignore") {
@@ -333,9 +310,8 @@ func GetAll(conn *sql.DB, object model.IModel, limit, offset int64) ([]interface
 
 		column := field.Tag.Get("column")
 		columns = append(columns, column)
-		pointers = append(pointers, rValue.Elem().Field(idx).Addr().Interface())
+		
 	}
-
 	var queryBuffer bytes.Buffer
 	var params []interface{}
 
@@ -350,43 +326,48 @@ func GetAll(conn *sql.DB, object model.IModel, limit, offset int64) ([]interface
 	}
 
 	query := queryBuffer.String()
-
-	row, err := conn.Query(query, params...)
-
+	row, err := conn.Query(query)// params...)
 	if nil != err {
 		log.Printf("Error conn.Query: %s\n\tError Query: %s\n", err.Error(), query)
 		return nil, err
 	}
-
 	defer row.Close()
 	objects := make([]interface{}, 0)
+	
+	cols, err := row.Columns() // Remember to check err afterwards
+	
+	
 	for row.Next() {
 		if nil != err {
 			log.Printf("Error row.Columns(): %s\n\tError Query: %s\n", err.Error(), query)
 			return nil, err
 		}
-
-		err = row.Scan(pointers...)
+		vals := make([]interface{}, len(cols))
+		writeCols := make([]string, len(cols))
+		for i, _ := range cols {
+			vals[i] = &writeCols[i]
+		}		
+		err = row.Scan(vals...)
 		if nil != err {
+
 			log.Printf("Error: row.Scan: %s\n", err.Error())
 			return nil, err
 		}
-
-		objects = append(objects, object)
+		objects = append(objects, vals)
 	}
-
+	
 	return objects, nil
 }
 
+
+
 func DeleteById(conn *sql.DB, object model.IModel, id int64) (sql.Result, error) {
-	//log.Printf("Preparing delete String.")
 	var queryBuffer bytes.Buffer
 	queryBuffer.WriteString("DELETE FROM ")
 	queryBuffer.WriteString(object.Table())
 	queryBuffer.WriteString(" WHERE id = ?")
 
 	query := queryBuffer.String()
-	//	log.Println("Delete statement is: %s", query)
 	stmt, err := conn.Prepare(query)
 	if nil != err {
 		log.Printf("Delete Syntax Error: %s\n\tError Query: %s : %s\n",
@@ -406,9 +387,6 @@ func DeleteById(conn *sql.DB, object model.IModel, id int64) (sql.Result, error)
 
 func SoftDeleteById(conn *sql.DB, object model.IModel, id int64) error {
 	var queryBuffer bytes.Buffer
-	//log.Printf("Soft Deletion String")
-
-
 	queryBuffer.WriteString("UPDATE ")
 	queryBuffer.WriteString(object.Table())
 	queryBuffer.WriteString(" SET deleted = 1  WHERE id = ?")
@@ -431,3 +409,4 @@ func SoftDeleteById(conn *sql.DB, object model.IModel, id int64) error {
 
 	return err
 }
+
